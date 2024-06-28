06063d9dab066e5e5f77db838fe62ba1a5c38fce
Bruno S.Rosa <brunos.rosa99@gmail.com>
2024-06-27 22:39:25 -0300

    Final da primeira aula

---
+++ main_01.py
@@ -0,0 +1,26 @@
+# Crie programa que o usuario digita o sue nome e retorno o numero de caracteris
+
+print(len(input("Digite o seu nome.: ")))
+
+#crie um programa aonde você ira digirar dois valores e ele trara o resultado
+
+print(int(input("Digite o primeiro numero ")) + int(input("Digite o segundo numero ")))
+
+#Variavei
+
+Numero = 3 # INT
+Numero_decimal = 3.14 #float
+Nome_de_usuario = "Bruno" # String
+Verdade = True #False / Bool
+
+print (type(Numero))
+print (type(Numero_decimal))
+print (type(Nome_de_usuario))
+print (type(Verdade))
+
+# Refatore o Exercicio02 atribuindo Variaveis
+#print(len(input("Gigite o seu nome; ")))
+
+Nome = input("Qaul o seu nome.: ")
+Quantidade_de_caracter = len(Nome)
+print(Quantidade_de_caracter)
