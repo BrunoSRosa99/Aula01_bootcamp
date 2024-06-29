3173374622ba6f0f00968eed14f6b9a9303558fa
Bruno S.Rosa <brunos.rosa99@gmail.com>
2024-06-29 16:46:34 -0300

    Adiocionar o desafio

---
+++ main.py
@@ -0,0 +1,14 @@
+print ("hello mundo")
+print (3)
+print (3 + 5)
+print ("Bruno" + " " + "Santa Rosa")
+
+# Ele pega apenas a informação.
+input ("Digite seu Nome: ")
+
+# Ele pega a informação e da um mensagem apos informar.
+print(input("Digite seu Nome: "))
+print("ola " + input("Digite o seu nome: " ))
+
+
+
